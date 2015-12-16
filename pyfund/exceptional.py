6d4d967fd2d6247d4615f2ee28168124749a868c
John A. Smith <john.a.smith@disney.com>
2015-12-16 08:00:28 -0500

    adding exercise file from PluralSight course.

---
+++ pyfund/exceptional.py
@@ -0,0 +1,16 @@
+''' A module for demonstrating exceptions '''
+import sys
+
+def convert(s):
+    ''' Convert to an integer '''
+
+    try:
+        x = int(s)
+        print("Conversion succeeded x = ", x)
+
+    except (ValueError, TypeError) as e:
+        print("Conversion error:{}"\
+              .format(str(e)),
+              file=sys.stderr)
+        raise
+
---
+++ pyfund/roots.py
@@ -0,0 +1,23 @@
+def sqrt(x):
+    ''' Compute square roots
+
+    :param x: number to square
+
+    :return: square of root x
+    '''
+
+    guess = x
+    i=0
+    while guess*guess != x and i < 20:
+        guess = (guess + x / guess) / 2.0
+        i+=1
+    return guess
+
+def main():
+    print(sqrt(9))
+    print(sqrt(2))
+
+if __name__ == '__main__':
+    main()
+
+
